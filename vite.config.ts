1a39797b8e6b7893d2637ed809e75eb536b2a9b5
Shane <shane@swalker.dev>
2024-03-14 22:49:40 -0500

    bumped remix version

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
-import mdx from "@mdx-js/rollup";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
-import rehypePrettyCode from "rehype-pretty-code";
+
+installGlobals();
 
 export default defineConfig({
-	plugins: [
-		remix(),
-		tsconfigPaths(),
-		mdx({
-			remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-			rehypePlugins: [rehypePrettyCode],
-		}),
-	],
+	plugins: [remix(), tsconfigPaths()],
 });
