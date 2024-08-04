f76011508688495fd838138603c6e221029bd017
Shane <shane@swalker.dev>
2024-08-03 22:13:33 -0500

    updated work header

--- app/components/WorkHeader.tsx
+++ app/components/WorkHeader.tsx
@@ -1,16 +1,21 @@
 import { format } from "date-fns";
-import { WorkFrontmatter } from "~/utils/works";
+import type { SelectWork } from "~/db/schema";
 
 export function WorkHeader({
-	frontmatter: { place, position, start, end, description },
-}: { frontmatter: WorkFrontmatter }) {
-	return (
-		<div className="not-prose dark:text-white">
-			<h1 className="text-3xl">{place}</h1>
-			<h2 className="font-light">{position}</h2>
-			<h3>
-				{format(start, "MMM yyy")} - {end ? format(end, "MMM yyy") : "Present"}
-			</h3>
-		</div>
-	);
-}
+		place,
+		position,
+		start,
+		end,
+		description,
+	}: Pick<SelectWork, "place" | "position" | "start" | "end" | "description">) {
+		return (
+			<div className="not-prose dark:text-white">
+				<h1 className="text-3xl">{place}</h1>
+				<h2 className="font-light">{position}</h2>
+				<h3>
+					{format(start, "MMM yyy")} -{" "}
+					{end ? format(end, "MMM yyy") : "Present"}
+				</h3>
+			</div>
+		);
+	}
